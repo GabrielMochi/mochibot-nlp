6371c4918412d12a5a783d6f44304579da6129ab
Gabriel Mochi <gmochi56@icloud.com>
2021-12-29 05:43:16 -0300

    creates a lib that contains all chat functions

---
+++ chat_functions.py
@@ -0,0 +1,50 @@
+from keras.models import load_model
+import nltk
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import pickle
+
+model = load_model("chatbot_model.h5")
+
+words = pickle.load(open("words.pkl", "rb"))
+classes = pickle.load(open("classes.pkl", "rb"))
+
+ERROR_THRESHOLD = 0.25
+
+
+def clean_up_sentences(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [WordNetLemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+
+def bow(sentence, show_details=True):
+    sentence_words = clean_up_sentences(sentence)
+    bag = [0] * len(words)
+
+    for sentence_word in sentence_words:
+        for i, word in words:
+            if word == sentence_word:
+                bag[i] = 1
+
+                if show_details:
+                    print("found in bag: %s" % word)
+
+    return np.array(bag)
+
+
+def predict_class(sentence):
+    bag = bow(sentence, show_details=False)
+    responses = model.predict(np.array([bag]))[0]
+    results = [[i, response] for i, response in enumerate(responses) if response >= ERROR_THRESHOLD]
+    return_list = []
+
+    results.sort(key=lambda x: x[1], reverse=True)
+
+    for result in results:
+        return_list.append({
+            "intent": classes[result[0]],
+            "probability": str(result[1])
+        })
+
+    return return_list
